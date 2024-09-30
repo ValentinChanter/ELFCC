705b9d03399c69ee92a06eaf80ff8510fb262254
ValentinChanter <singvalent@cy-tech.fr>
2024-09-30 17:09:36 +0200

    Exercice 2

--- Exercices/ex.s
+++ Exercices/ex.s
@@ -1,30 +1,51 @@
 section .data
-    filename db "test.txt", 0
-    buffer times 32 db 0
-    len equ 32
+    filename db "uppercase", 0
+    elf db 0x7F, 'E', 'L', 'F'
+    buffer times 4 db 0
+    len equ 4
+
+    not_elf_msg db "This file does not have a ELF header.", 10
+    not_elf_len equ $ - not_elf_msg
 
 section .text
     global _start
 
 _start:
-    ; sys_open
+    ; Open the file
     mov rax, 2
     mov rdi, filename
     mov rsi, 0          ; O_RDONLY (read-only)
     syscall
     mov rdi, rax        ; file descriptor
 
-    ; sys_read
+    ; Read the file contents
     mov rax, 0          
     mov rsi, buffer
     mov rdx, len
     syscall
 
-    ; sys_write
+    ; Check if it has a ELF header
+    mov rdi, buffer
+    mov ri, elf        
+    mov rcx, 4         
+    repe cmpsb          
+    jne not_elf
+
+    jmp close
+
+not_elf:
+    ; Display an error message
     mov rax, 1          
     mov rdi, 1
-    mov rsi, buffer
-    mov rdx, len
+    mov rsi, not_elf_msg
+    mov rdx, not_elf_len
+    syscall
+
+    jmp close
+
+close:
+    ; Close the file
+    mov rax, 3
     syscall
 
     mov rax, 60
